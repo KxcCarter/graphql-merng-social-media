18f2024ef448c1769ca0731a4700af5b36e6f96e
Kenneth Carter <kxccarter@gmail.com>
2021-02-05 12:52:38 -0600

    setup login and login validation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,12 +2,56 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
-const { validateRegisterInput } = require('../../util/validators');
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require('../../util/validators');
 const User = require('../../models/User');
 const { SECRET_KEY } = require('../../config');
 
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+}
+
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError('Error', { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = 'User not found.';
+        throw new UserInputError('You do not exist', { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = 'Wrong Credentials';
+        throw new UserInputError('Wrong Credentials', { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } }
@@ -42,15 +86,8 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: '1h' }
-      );
+      const token = generateToken(res);
+
       return {
         ...res._doc,
         id: res._id,
