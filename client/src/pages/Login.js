77403617d57db144718d7c91d4230c768d76c2a7
Kenneth Carter <kxccarter@gmail.com>
2021-02-08 18:44:02 -0600

    creates a context for authenticating users

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Button, Form } from 'semantic-ui-react';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
+import { AuthContext } from '../context/auth';
 import { useForm } from '../util/hooks';
 
 const Login = (props) => {
+  const context = useContext(AuthContext);
+  const [errors, setErrors] = useState({});
   const { onChange, onSubmit, values } = useForm(loginUserCallback, {
     username: '',
     password: '',
   });
 
-  const [errors, setErrors] = useState({});
-
   const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-    update(proxy, result) {
-      console.log(result);
+    update(_, { data: { login: userData } }) {
+      context.login(userData);
       props.history.push('/');
     },
     onError(err) {
